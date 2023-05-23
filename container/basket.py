7a0029bf8850253141d918c3ca7e56ea41101ad4
AlexPronkevich <aa-kk@yandex.ru>
2023-05-23 21:59:50 +0300

    Добавили абстракцию

--- container/basket.py
+++ container/basket.py
@@ -1,3 +1,6 @@
+from entity.product import Product
+
+
 class Basket:
     def __init__(self, products=None):
         if not products:
@@ -6,29 +9,28 @@ class Basket:
             self.__products = products
 
         # if products:
-
-
+              #self.__products
 
     # def get_size(self):
     #     return len(self.__products)
 
-    @property                             # Это свойство вместо метода выше
+    @property  # Это свойство вместо метода выше
     def size(self):
         return len(self.__products)
 
     def get_product(self, index):
-        return self.__products[index]
+        if (isinstance(index, int) and
+                index >= 0 and index < self.size):
+            return self.__products[index]
 
     def add(self, product):
-        self.__products.append(product)
+        if isinstance(product, (Bread, Milk, Orange)):
+            self.__products.append(product)
 
     def __str__(self):
         msg = "List of products:"
 
-        for i in range (self.size):
-            msg += f"\n{i+1} " + str(self.__products[i])
+        for i in range(self.size):
+            msg += f"\n{i + 1} " + str(self.__products[i])
 
         return msg
-
-
-
--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from entity.milk import Milk
 from entity.orange import Orange
 from entity.bread import Bread
 from container.basket import Basket
+from logic.shop_assistance import ShopAssistance
 
 
 def main():
     basket = Basket()
-    br = Bread()
-    o = Orange()
+    br = Bread("white", "second", 3.0)
+    o = Orange(200, 300, 1.5)
     m = Milk(1, 4.2, 5.5)
 
     basket.add(br)
@@ -17,6 +18,8 @@ def main():
     # print(f"size = {basket.size}")
 
     print(basket)
+
+    total = ShopAssistance.calculate_total_price(Basket)
 
     # for i in range(basket.size):
     #     print(basket.get_product(i))
